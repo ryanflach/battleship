85d7da4e4e9052aded7d6442be693de5b6b56e61
Ryan Flach <ryanflach@users.noreply.github.com>
2016-05-25 13:23:29 -0600

    Created Validation module for all placement validation methods. Created appropriate tests.

---
+++ lib/validation.rb
@@ -0,0 +1,87 @@
+module Validation
+  def all_adjacent_locations(previous_location)
+    up_letter = "#{previous_location[0].next}#{previous_location[1]}"
+    up_number = "#{previous_location[0]}#{previous_location[1].next}"
+    down_letter = "#{(previous_location[0].ord - 1).chr}#{previous_location[1]}"
+    down_number = "#{previous_location[0]}#{(previous_location[1].ord - 1).chr}"
+    possibilities = [up_letter, up_number, down_letter, down_number]
+  end
+
+  def random_option(possibilities)
+    possibilities.delete_at(rand(possibilities.length))
+  end
+
+  def check_for_third_spot(existing_locations)
+    if existing_locations[0][0] == existing_locations[1][0]
+      horizontal_placement(existing_locations)
+    else
+      vertical_placement(existing_locations)
+    end
+  end
+
+  def horizontal_placement(existing_locations)
+    if existing_locations[0][1] == '1'
+      final_location = existing_locations[0][0] + '3'
+    elsif existing_locations[0][1] == '3'
+      final_location = existing_locations[0][0] + '2'
+    else
+      final_location = existing_locations[0][0] + %w(1 4).sample
+    end
+  end
+
+  def vertical_placement(existing_locations)
+    if existing_locations[0][0] == 'A'
+      final_location = 'C' + existing_locations[0][1]
+    elsif existing_locations[0][0] == 'C'
+      final_location = 'B' + existing_locations[0][1]
+    else
+      final_location = %w(A D).sample + existing_locations[0][1]
+    end
+  end
+
+  def position_wrong_format_or_outside_range?(locations)
+    !locations.all? do |place|
+      place[0].between?('A', 'D') && place[1].between?('1', '4')
+    end
+  end
+
+  def position_taken?(locations, player)
+    !locations.all? do |place|
+      player.boards.my_ships.valid_locations.include?(place)
+    end
+  end
+
+  def positions_include_duplicates?(locations)
+    true if locations.uniq.size < locations.size
+  end
+
+  def position_wraps?(locations)
+    if contains(locations, 'A') && contains(locations, 'D')
+      true
+    elsif contains(locations, '1') && contains(locations, '4')
+      true
+    else
+      false
+    end
+  end
+
+  def contains(locations, char)
+    locations.any? { |place| place[0] == char }
+  end
+
+  def positions_not_adjacent?(locations)
+    outcome = false
+    if locations.size < 2
+      outcome
+    else
+      index = 0
+      while index < locations.size - 1
+        if !all_adjacent_locations(locations[index]).include?(locations[index + 1])
+          outcome = true
+        end
+        index += 1
+      end
+    end
+    outcome
+  end
+end
---
+++ test/validation_test.rb
@@ -0,0 +1,92 @@
+require './test/test_helper'
+require './lib/validation'
+require './lib/player'
+
+class ValidationTest < Minitest::Test
+  include Validation
+
+  def test_it_can_find_and_return_all_adjacent_locations
+    result = all_adjacent_locations('A1')
+    possibilities = ['B1', 'A2', '@1', 'A0']
+    assert_equal possibilities, result
+  end
+
+  def test_it_can_find_a_valid_horizontal_spot_for_a_size_three_ship_with_one_option
+    result = check_for_third_spot(%w(A1 A2))
+    assert_equal 'A3', result
+    result = check_for_third_spot(%w(B3 B4))
+    assert_equal 'B2', result
+  end
+
+  def test_it_can_find_a_valid_vertical_spot_for_a_size_three_ship_with_one_option
+    result = check_for_third_spot(%w(C3 D3))
+    assert_equal 'B3', result
+    result = check_for_third_spot(%w(A1 B1))
+    assert_equal 'C1', result
+  end
+
+  def test_it_can_find_a_valid_random_horizontal_third_spot_for_a_size_three_ship
+    result = check_for_third_spot(%w(A2 A3))
+    possibilities = %w(A1 A4)
+    assert possibilities.include?(result)
+  end
+
+  def test_it_can_find_a_valid_random_vertical_third_spot_for_a_size_three_ship
+    result = check_for_third_spot(%w(B2 C2))
+    possibilities = %w(A2 D2)
+    assert possibilities.include?(result)
+  end
+
+  def test_it_knows_if_a_single_input_is_in_the_wrong_format_or_off_the_board
+    result = position_wrong_format_or_outside_range?(['A9'])
+    assert result
+    result = position_wrong_format_or_outside_range?(['1B'])
+    assert result
+    result = position_wrong_format_or_outside_range?(['A2'])
+    refute result
+  end
+
+  def test_it_knows_if_multiple_inputs_are_wrong_format_or_off_board
+    result = position_wrong_format_or_outside_range?(%w(A1 A2))
+    refute result
+    result = position_wrong_format_or_outside_range?(%w(A1 B5))
+    assert result
+  end
+
+  def test_it_can_tell_if_positions_are_available
+    player = Player.new
+    result = position_taken?(%w(A1 A2), player)
+    refute result
+    player.boards.my_ships.valid_locations.delete('A1')
+    result = position_taken?(['A1'], player)
+    assert result
+  end
+
+  def test_it_can_detect_duplicates_in_an_input_of_locations
+    result = positions_include_duplicates?(%w(A1 A2 A3))
+    refute result
+    result = positions_include_duplicates?(%w(A1 A2 A2))
+    assert result
+  end
+
+  def test_it_can_tell_if_an_input_contains_a_specific_character
+    result = contains(%w(A1 A2), 'A')
+    assert result
+    result = contains(%w(A1 A2), '4')
+    refute result
+  end
+
+  def test_it_can_tell_if_an_input_wraps_around_the_board
+    result = position_wraps?(%w(A1 D1))
+    assert result
+    result = position_wraps?(%w(B2 C2))
+    refute result
+  end
+
+  def test_it_can_tell_if_inputs_are_diagnonal_or_non_adjacent
+    result = positions_not_adjacent?(['A2', 'C3'])
+    assert result
+    result = positions_not_adjacent?(['A2', 'A3'])
+    refute result
+  end
+end
