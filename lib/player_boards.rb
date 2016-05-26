c7059bc04d4619f5f688d845efec1a0967d0b71c
Ryan Flach <ryanflach@users.noreply.github.com>
2016-05-25 18:10:13 -0600

    Moved transpose_letter method to PlayerBoards class and added test

--- lib/player_boards.rb
+++ lib/player_boards.rb
@@ -8,4 +8,22 @@ class PlayerBoards
     @my_ships = GameBoard.new
     @my_hits_and_misses = GameBoard.new
   end
+
+  def add_hit_or_miss(location, hit_or_miss)
+    index = transpose_letter(location[0])
+    my_hits_and_misses.board[index][location[1].to_i] = hit_or_miss
+  end
+
+  def transpose_letter(letter)
+    index = 2
+    if letter == 'B'
+      index = 3
+    elsif letter == 'C'
+      index = 4
+    elsif letter == 'D'
+      index = 5
+    else
+      index
+    end
+  end
 end
--- test/player_boards_test.rb
+++ test/player_boards_test.rb
@@ -13,4 +13,17 @@ class PlayerBoardsTest < Minitest::Test
   def test_it_has_a_board_to_track_its_hits_and_misses
     assert @players_boards.my_hits_and_misses
   end
+
+  def test_it_can_convert_a_letter_into_its_appropriate_board_index
+    assert_equal 2, @players_boards.transpose_letter('A')
+  end
+
+  def test_it_can_add_a_hit_to_its_hits_and_misses_board
+    hit_index = @players_boards.transpose_letter('A')
+    miss_index = @players_boards.transpose_letter('C')
+    hit = @players_boards.add_hit_or_miss('A1', 'H')
+    miss = @players_boards.add_hit_or_miss('C2', 'M')
+    assert_equal 'H', @players_boards.my_hits_and_misses.board[hit_index][1]
+    assert_equal 'M', @players_boards.my_hits_and_misses.board[miss_index][2]
+  end
 end
